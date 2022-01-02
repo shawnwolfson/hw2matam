08c64e9bf0c492cb6894f0164bf5e79f0495eed6
shawnwolfson <96698222+shawnwolfson@users.noreply.github.com>
2022-01-02 22:11:37 +0200

    update

--- Skill.h
+++ Skill.h
@@ -13,13 +13,14 @@ using std::cout;
 namespace mtm{
 class Skill
 {
-    int skill_id;
-    string skill_name;
+    int id;
+    string name;
     int points_to_acquire_skill;
 
 public:
     //constructors
-    Skill(int skill_id, string skill_name, int points_to_acquire_skill);
+    Skill() = default;
+    Skill(int id, string name, int points_to_acquire_skill);
     Skill(const Skill& skill);
     ~Skill() = default;
 
@@ -33,12 +34,12 @@ public:
     Skill& operator+= (const int points); 
     Skill& operator= (const Skill& skill);
     void operator++(int);
-    bool operator< (const Skill skill_to_compare_with);
-    bool operator> (const Skill skill_to_compare_with);
-    bool operator== (const Skill skill_to_compare_with);
-    bool operator<= (const Skill skill_to_compare_with);
-    bool operator>= (const Skill skill_to_compare_with);
-    bool operator!= (const Skill skill_to_compare_with);
+    bool operator< (const Skill skill_to_compare_with) const;
+    bool operator> (const Skill skill_to_compare_with) const;
+    bool operator== (const Skill skill_to_compare_with) const;
+    bool operator<= (const Skill skill_to_compare_with) const;
+    bool operator>= (const Skill skill_to_compare_with) const;
+    bool operator!= (const Skill skill_to_compare_with) const;
 
     //class friends
     friend ostream& operator<< (ostream& os, const Skill skill);
@@ -52,4 +53,3 @@ Skill operator+ (const int points, const Skill& skill); // needs to be a symmetr
 
 }
 #endif /* SKILL_H_ */
-
