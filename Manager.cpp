a365dd5d0a220faec2cc21bdf974e15af5dad828
shawnwolfson <96698222+shawnwolfson@users.noreply.github.com>
2022-01-03 19:40:21 +0200

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -1,36 +1,22 @@
 #include "Manager.h"
 #include "exceptions.h"
 using namespace mtm;
-using std::set;
-
-bool findEmployeeById(std::set<Employee> employee_group, int id) 
-{
-    set<Employee>::iterator iterator;
-    for(iterator = employee_group.begin(); iterator != employee_group.end(); ++iterator)
-    {
-        if((*iterator).getId() == id)
-        {
-            return true;
-        }
-    }
-    return false;
-}
 
 
-Manager::Manager(int id, string first_name, string last_name, int birth_year, double score, double salary, set<Employee> employee_group):
-                 Citizen(id, first_name, last_name, birth_year), score(score), salary(salary), employee_group(employee_group) 
+Manager::Manager(int id, string first_name, string last_name, int birth_year, double score, double salary, bool hired, set<Employee> employee_group):
+                 Citizen(id, first_name, last_name, birth_year), score(score), salary(salary), hired(hired) ,employee_group(employee_group) 
                 {}
 
 
 Manager::Manager(const Manager& manager):
-                Citizen(manager), salary(manager.salary), score(manager.score)
+                Citizen(manager), salary(manager.salary), score(manager.score), hired(hired)
                 {
                     employee_group = manager.employee_group;
                 }
 
 void Manager::addEmployee(Employee* employee)
 {
-    if(!((this->employee_group.find(*employee)) == this->employee_group.end())) //this means the employee is already in the group
+    if(!((this->employee_group.find(*employee)) == (this->employee_group.end()))) //this means the employee is already in the group
     {
         throw EmployeeAlreadyHired();
     }
@@ -39,7 +25,7 @@ void Manager::addEmployee(Employee* employee)
 
 void Manager::removeEmployee(int id) 
 {
-    if(!findEmployeeById(this->employee_group, id))
+    if(!(mtm::findCitizenInGroupById<Employee>(this->employee_group, id)))
     {
         throw EmployeeIsNotHired();
     }
@@ -61,6 +47,11 @@ double Manager::getSalary()
 void Manager::setSalary(int raise)
 {
     (this->salary) += raise;
+}
+
+bool Manager::isHired()
+{
+    return this->hired;
 }
 
 ostream& Manager::printShort(ostream& os)
@@ -94,8 +85,3 @@ Manager* Manager::clone() const //is not complete
     return &new_manager;
 }
 
-
-int main()
-{
-    return 0;
-}
