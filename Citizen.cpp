8d63112c44a3f3bf4905ba978d39a71743d980d4
shawnwolfson <96698222+shawnwolfson@users.noreply.github.com>
2022-01-05 18:44:02 +0200

    Update Citizen.cpp

--- Citizen.cpp
+++ Citizen.cpp
@@ -1,4 +1,6 @@
 #include "Citizen.h"
+using std::endl;
+using std::cout;
 using namespace mtm;
 
 //**c'tors, d'tor**//
@@ -6,70 +8,55 @@ Citizen::Citizen(int id, string first_name, string last_name, int birth_year) :
     id(id), first_name(first_name), last_name(last_name), birth_year(birth_year)
 {}
 
-Citizen::Citizen(const Citizen &citizen) : 
-    id(citizen.id), first_name(citizen.first_name), last_name(citizen.last_name), birth_year(citizen.birth_year)
-{}
-
 //**getters**//
 int Citizen::getId() const
 {
-    return this->id;
+    return id;
 }
 
 string Citizen::getFirstName() const
 {
-    return this->first_name;
+    return first_name;
 }
 
 string Citizen::getLastName() const
 {
-    return this->last_name;
+    return last_name;
 }
 
 int Citizen::getBirthYear() const
 {
-    return this->birth_year;
+    return birth_year;
 }
 
 //**operators**//
-/*Citizen& operator=(const Citizen& citizen)
-{
-    if(this == &citizen) {
-        return *this;
-    }
-    id = citizen.id;
-    first_name = citizen.first_name;
-    last_name = citizen.last_name;
-    birth_year = citizen.birth_year;
-    return *this;
-}*/
-
 bool Citizen::operator<(const Citizen& citizen) const
 {
-    return ((this->getId() < citizen.getId()) ? true : false);
+    return ((getId() < citizen.getId()) ? true : false);
 }
 
 bool Citizen::operator>(const Citizen& citizen) const
 {
-    return ((this->getId() > citizen.getId()) ? true : false);
+    return ((getId() > citizen.getId()) ? true : false);
 }
 
 bool Citizen::operator==(const Citizen& citizen) const
 {
-    return ((this->getId() == citizen.getId()) ? true : false);
+    return ((getId() == citizen.getId()) ? true : false);
 }
 
 bool Citizen::operator<=(const Citizen& citizen) const
 {
-    return ((this->getId() <= citizen.getId()) ? true : false);
+    return ((getId() <= citizen.getId()) ? true : false);
 }
 
 bool Citizen::operator>=(const Citizen& citizen) const
 {
-    return ((this->getId() >= citizen.getId()) ? true : false);
+    return ((getId() >= citizen.getId()) ? true : false);
 }
 
 bool Citizen::operator!=(const Citizen& citizen) const
 {
-    return ((this->getId() != citizen.getId()) ? true : false);
+    return ((getId() != citizen.getId()) ? true : false);
 }
+
