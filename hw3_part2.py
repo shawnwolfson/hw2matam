bd3c83cce96edcfbb37e32667034ab3eb43c6505
shawnwolfson <96698222+shawnwolfson@users.noreply.github.com>
2022-01-19 11:49:43 +0200

    Update hw3_part2.py

--- hw3_part2.py
+++ hw3_part2.py
@@ -1,15 +1,15 @@
 #Part 3.1
 
-#Function to check if a string is a palindrome
+#Function to check if a string is a palindrome.
 def is_palindrome(str):
     reversed_string = ''.join(reversed(str))
     return (reversed_string == str)
 
-#Function to get all substrings of a given length of a string
+#Function to get all substrings of a given length of a string.
 def get_all_substrings_in_a_given_length(str, length):
     return [str[start_index: start_index + length] for start_index in range(len(str) - length + 1)]
 
-#Function to set the values that will go into the dictionary
+#Function to set the values that will go into the dictionary.
 def get_dict_values(str, length):
     substrings = get_all_substrings_in_a_given_length(str, length)
     values = []
@@ -18,6 +18,7 @@ def get_dict_values(str, length):
           values.append(element)
     return values
 
+#The requested function.
 def get_palindrom_dict(str):
     new_dict = {}
     if (str == ""):
@@ -29,27 +30,66 @@ def get_palindrom_dict(str):
             new_dict[t] = values
     return new_dict
 
-dict1 = get_palindrom_dict("AbbAcaBBa")
-for k, v in dict1.items():
-    print(k , 'is for' , v)
-print("-----------")
-dict2 = get_palindrom_dict("aaa")
-for k, v in dict2.items():
-    print(k , 'is for' , v)
-print("-----------")
-dict3 = get_palindrom_dict("horse")
-for k, v in dict3.items():
-    print(k , 'is for' , v)
-print("-----------")
-dict4 = get_palindrom_dict("aAaAa")
-for k, v in dict4.items():
-    print(k , 'is for' , v)
-print("-----------")
-dict5 = get_palindrom_dict("   ")
-for k, v in dict5.items():
-    print(k , 'is for' , v)
-print("-----------")
-dict6 = get_palindrom_dict("9")
-for k, v in dict6.items():
-    print(k , 'is for' , v)
-print("-----------")
+#dict1 = get_palindrom_dict("AbbAcaBBa")
+#for k, v in dict1.items():
+#    print(k , 'is for' , v)
+#print("-----------")
+#dict2 = get_palindrom_dict("aaa")
+#for k, v in dict2.items():
+#    print(k , 'is for' , v)
+#print("-----------")
+#dict3 = get_palindrom_dict("horse")
+#for k, v in dict3.items():
+#    print(k , 'is for' , v)
+#print("-----------")
+#dict4 = get_palindrom_dict("aAaAa")
+#for k, v in dict4.items():
+#    print(k , 'is for' , v)
+#print("-----------")
+#dict5 = get_palindrom_dict("   ")
+#for k, v in dict5.items():
+#    print(k , 'is for' , v)
+#print("-----------")
+#dict6 = get_palindrom_dict("9")
+#for k, v in dict6.items():
+#    print(k , 'is for' , v)
+#print("-----------")
+
+#--------------------------------------------------------------------#
+#Part 3.2
+
+#Function to split the given strings into a list of 2 strings as needed.
+def get_odd_and_even_strings(str):
+    new_strings = []
+    new_strings.append(str[::2])
+    new_strings.append(str[1::2])
+    return new_strings
+
+#Function to check is the two strings that the list contains are similiar by the definition given.
+def check_if_similar(strings_to_check):
+    iteratable_string = strings_to_check[0]
+    static_string = strings_to_check[1]
+    #This dictionary maps the changes of the letters.
+    switched_paires_dict = {}
+    for index in range(len(static_string)):
+        if(iteratable_string[index] in switched_paires_dict):
+            if(switched_paires_dict[iteratable_string[index]] != static_string[index]):
+                return False
+        switched_paires_dict[iteratable_string[index]] = static_string[index]
+    return True
+
+def check_match(str):
+    if(str == ""):
+        return True
+    strings_to_check = get_odd_and_even_strings(str)
+    if(len(strings_to_check[0]) != len(strings_to_check[1])):
+        return False
+    return (check_if_similar(strings_to_check))
+
+#assert check_match("dkoeoerp")
+#assert not check_match("dkoeomrp")
+#assert check_match("kdeoeopr")
+#assert not check_match("kdeoempr")
+#assert check_match("sdaadd")
+#assert not check_match("dsaadd")
+#assert not check_match("chheelcpk")
